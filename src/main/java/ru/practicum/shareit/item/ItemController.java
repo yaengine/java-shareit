f85a94652c2de61bec9880002feddb71d9325f30
yaengine <yaengine@yandex.ru>
2025-04-27 01:17:26 +0300

    Sprint 14. ShareIt. Add constant for header. ToReview.

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -20,33 +20,35 @@ public class ItemController {
     private final ItemService itemService;
     private final UserService userService;
 
+    private static final String X_SHARER_USER_ID = "X-Sharer-User-Id";
+
     @PostMapping
-    public ItemDto createItem(@RequestHeader("X-Sharer-User-Id") Long userId, @RequestBody @Valid ItemDto itemDto) {
+    public ItemDto createItem(@RequestHeader(X_SHARER_USER_ID) Long userId, @RequestBody @Valid ItemDto itemDto) {
         UserDto userDto = userService.findUserById(userId);
         return itemService.createItem(itemDto, userDto);
     }
 
     @PatchMapping("/{itemId}")
-    public ItemDto updateItemById(@RequestHeader("X-Sharer-User-Id") Long userId, @RequestBody ItemDto itemDto,
+    public ItemDto updateItemById(@RequestHeader(X_SHARER_USER_ID) Long userId, @RequestBody ItemDto itemDto,
                                   @PathVariable("itemId") Long itemId) {
         userService.findUserById(userId);
         return itemService.updateItemById(itemDto, userId, itemId);
     }
 
     @GetMapping("/{itemId}")
-    public ItemDto findItemById(@RequestHeader("X-Sharer-User-Id") Long userId, @PathVariable("itemId") Long itemId) {
+    public ItemDto findItemById(@RequestHeader(X_SHARER_USER_ID) Long userId, @PathVariable("itemId") Long itemId) {
         userService.findUserById(userId);
         return itemService.findItemById(itemId);
     }
 
     @GetMapping
-    public Collection<ItemDto> findAllByUserId(@RequestHeader("X-Sharer-User-Id") Long userId) {
+    public Collection<ItemDto> findAllByUserId(@RequestHeader(X_SHARER_USER_ID) Long userId) {
         userService.findUserById(userId);
         return itemService.findAllByUserId(userId);
     }
 
     @GetMapping("/search")
-    public Collection<ItemDto> searchItemsByText(@RequestHeader("X-Sharer-User-Id") Long userId,
+    public Collection<ItemDto> searchItemsByText(@RequestHeader(X_SHARER_USER_ID) Long userId,
                                                  @RequestParam(name = "text") String text) {
         userService.findUserById(userId);
         return itemService.searchItemsByText(text);
