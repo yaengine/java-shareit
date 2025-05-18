0a9a29960461b85bc919c24f5b3eec86f3e6e781
yaengine <yaengine@yandex.ru>
2025-05-18 14:37:39 +0300

    Sprint 15. ShareIt. To Review.

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, classes = ShareItApp.class)
 class ShareItTests {
 
 	@Test
